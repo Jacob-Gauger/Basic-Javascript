b30874c5df3526caf6e197e5b8bdaf2d5d4fc9a1
Jacob-Gauger <ja312059@ucf.edu>
2026-02-17 12:56:02 -0500

    Most of it done

---
+++ js.js
@@ -0,0 +1,48 @@
+let num = 0;
+let index;
+
+function tickUp(){
+    num+= 1;
+    document.getElementById("counter").innerHTML = num;
+}
+
+function tickDown(){
+    num-= 1;
+    document.getElementById("counter").innerHTML = num;
+}
+
+function runForLoop(){
+    for(let i = 0; i < (num +1); i++){
+        document.getElementById("forLoopResult").innerHTML += i + " ";
+    }
+}
+
+function showOddNumbers(){
+    for(let i = 1; i < (num +1); i+=2){
+        document.getElementById("oddNumberResult").innerHTML += i + " ";
+    }
+}
+
+function addMultiplesToArray(){
+    const multiples = [];
+}
+
+function printCarObject(){
+
+}
+
+function loadCar(index){
+    
+}
+
+function changeColor(index){
+    if (index == 1){
+        document.getElementById("styleParagraph").style.color = "red";
+    }
+    if (index == 2){
+        document.getElementById("styleParagraph").style.color = "green";
+    }
+    if (index == 3){
+        document.getElementById("styleParagraph").style.color = "blue";
+    }
+}
